b6e04dba766fed2db11d07dffefa10238c6f7fbf
Roshan Kumar Singh <roshan5123@outlook.in>
2022-03-19 18:59:11 +0530

    strings

---
+++ 2_strings.py
@@ -0,0 +1,30 @@
+msg=("Python is an easy language")
+
+#1
+
+#you can change the case of letters through string methods.
+
+print(msg.title())
+
+print(msg.lower())
+
+print(msg.upper())
+
+#string methods are functions
+
+print("****************************************************")
+
+#2
+#String Concatenation
+
+f_name="Tony"
+l_name="Stark"
+
+full_name=f_name+" " +l_name + " is Iron Man."
+
+print(full_name)
+
+print("*****************************************************")
+print("Python")
+print("\tPython")
+
