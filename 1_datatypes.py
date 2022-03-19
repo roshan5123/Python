1f167ea9536344ed2ee553938c39cb6c2faab530
Roshan Kumar Singh <roshan5123@outlook.in>
2022-03-19 17:52:59 +0530

    Data Types

---
+++ 1_datatypes.py
@@ -0,0 +1,58 @@
+print("I am learning Python")
+
+
+print("*********************************")
+
+msg=("Python is easy")
+
+print(msg)
+
+print("**********************************")
+
+#This is Python string
+a=str("I am learning Python from scratch.")
+print(a)
+
+print("***************************************************")
+#This is Python int datatype
+b=int(40)
+print(b)
+
+print("***************************************************")
+
+#This is Python float
+c=float(30.777)
+print(c)
+
+print("*************************************************")
+
+#This is Python list
+d = list(("Great expectations", "God of flies", "God of small things", "Tempest", "Othello"))
+print(d)
+
+print("*********************************************************")
+
+#This is Python tuple
+e=(("Great expectations", "God of flies", "God of small things", "Tempest", "Othello"))
+print(e)
+
+print("**************************************************")
+
+#This is Python Range
+f=range(7)
+print(f)
+for n in f:
+    print(n)
+
+print("***************************************************")
+
+#This is Python set
+
+g=set(("MI","CSK","KKR","SRH","RR","RCB","DC","PBKS","GT","LSJ"))
+print(g)
+print("**************************************************")
+
+h = frozenset(("Great expectations", "God of flies", "God of small things", "Tempest", "Othello"))
+print(h)
+
+print("***************************************************")
